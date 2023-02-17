7551a55cc2cf360453c543becc4d6b2030fa61ae
Ardell Moiko <moikoardell@gmail.com>
2023-02-17 13:25:00 -0800

    print except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x, y;
+for (x = 'a'; x <= 'z'; x++)
+{
+if (x != e && x != q)
+{
+putchar(x);
+}
+else
+{
+puthcar('\n');
+}
+}
+  return (0);
+}
