8fdd101eae193a1c0f3ec5de8aaf951d2173ac0d
Ardell Moiko <moikoardell@gmail.com>
2023-02-17 14:53:58 -0800

    double digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x, y;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+if (x != y)
+{
+putchar('0' + x);
+putchar('0' + y);
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
