4b4d00985c742ec92c4a33d695da671b7164e3f3
Ardell Moiko <moikoardell@gmail.com>
2023-02-17 07:26:11 -0800

    print lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 /**
  *main - Entry point
  *
@@ -7,7 +8,7 @@
 
 int main(void)
 {
-char sl = toLower('M');
+char sl = tolower('M');
 putchar(sl);
 return (0);
 }
