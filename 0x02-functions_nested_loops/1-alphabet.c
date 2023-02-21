4d13f8801e1e72c843ce8ec3967353c4f1d0f2c8
Ardell Moiko <moikoardell@gmail.com>
2023-02-21 05:21:03 -0800

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+return (0);
+}
