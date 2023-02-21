c0e8f693e7b564c77b26d843df2e6d9891ed04f2
Ardell Moiko <moikoardell@gmail.com>
2023-02-21 06:03:25 -0800

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  * Return: Always 0.
  */
 
-int main(void)
+void print_alphabet(void)
 {
 int x;
 for (x = 'a'; x <= 'z'; x++)
@@ -14,5 +14,4 @@ for (x = 'a'; x <= 'z'; x++)
 _putchar(x);
 }
 _putchar('\n');
-return (0);
 }
