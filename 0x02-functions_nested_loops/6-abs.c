7063b21d59694c20cdaa7145af7b4146ec988434
Ardell Moiko <moikoardell@gmail.com>
2023-02-21 10:24:52 -0800

    absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - check the code
+ * @x: the integar to be taken.
+ * Return: Always 0.
+ */
+int _abs(int x)
+{
+_putchar('0' + abs(x));
+return (0);
+}
