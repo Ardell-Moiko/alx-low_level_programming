50a03c258d1d86c94f2c9c44e3946e1bb69a8dd3
Ardell Moiko <moikoardell@gmail.com>
2023-03-02 17:58:20 -0800

    Capitalize each word in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalize each word
+ * @s: string var
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+int i = 0;
+
+while (s[i])
+{
+while (!(s[i] >= 97 && s[i] <= 122))
+i++;
+if (s[i - 1] == ' ' ||
+s[i - 1] == '\t' ||
+s[i - 1] == '\n' ||
+s[i - 1] == ',' ||
+s[i - 1] == ';' ||
+s[i - 1] == '.' ||
+s[i - 1] == '!' ||
+s[i - 1] == '?' ||
+s[i - 1] == '"' ||
+s[i - 1] == '(' ||
+s[i - 1] == ')' ||
+s[i - 1] == '{' ||
+s[i - 1] == '}' ||
+i == 0)
+s[i] -= 32;
+i++;
+}
+return (s);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - encodes a string to 1337
+ * @str: string var.
+ * Return: Always 0.
+ */
+char *leet(char *str)
+{
+    return (str);
+}
