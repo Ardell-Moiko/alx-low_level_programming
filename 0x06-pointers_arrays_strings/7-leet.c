686be59104fab147446b8660937a0390513b038d
Ardell Moiko <moikoardell@gmail.com>
2023-03-02 18:14:58 -0800

    Encode string to 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,7 +6,19 @@
  * @str: string var.
  * Return: Always 0.
  */
-char *leet(char *str)
+char *leet(char *s)
 {
-    return (str);
+char a[] = { 'a', 'e', 'o', 't', 'l' };
+char n[] = { 4, 3, 0, 7, 1 };
+int i = 0;
+while (*s)
+{
+for (i = 0; i < 5; i++)
+{
+if (*s == a[i] || *s == a[i] - 32)
+*s = n[i] + '0';
+}
+s++;
+}
+return (s);
 }
