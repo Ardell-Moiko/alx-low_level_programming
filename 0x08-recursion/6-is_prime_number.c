05245b045d3941dfaed5f942fac53bfe0ba525db
Ardell Moiko <moikoardell@gmail.com>
2023-03-08 09:33:45 -0800

    Returns x to power y using recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - Checks if a num is a prime number
+ * @n: integer var
+ * Return: one or zero
+ */
+int is_prime_number(int n)
+{
+    return (0);
+}
