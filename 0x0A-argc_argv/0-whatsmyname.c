42af60c594fcd79c4e9b739e5ccab6aa9e344620
Ardell Moiko <moikoardell@gmail.com>
2023-03-10 09:38:19 -0800

    Print name of program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_name - prints the name of the program.
+ * main - prints the name of the program.
  * @argc: number of commands entered in argv
  * @argv: value of commands entered
  * Return: Always 0
@@ -9,11 +9,11 @@
 
 int main(int argc, char* argv[])
 {
-  int i;
+int i;
 
-  for (i = 0; i < argc; i++)
-    {
-      printf("%s\n", argv[i]);
-    }
-  return (0);
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
