cc19ac54abafe15b972786050fb38d15e4254494
Ardell Moiko <moikoardell@gmail.com>
2023-03-10 16:42:21 -0800

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,20 +12,20 @@ int main(int argc, char *argv[])
 {
 int i, sum = 0;
 
-if (argc <= 1)
+if (argc > 1)
 {
-printf("0\n");
-return (0);
-}
 for (i = 1; i < argc; i++)
 {
 if (*argv[i] < 48 || *argv[i] > 57)
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
+}
+else
+printf("0\n");
 return (0);
 }
