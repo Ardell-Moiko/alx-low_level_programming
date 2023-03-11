f685e4c464e03c5699aadb98ee72dd6b674bcec3
Ardell Moiko <moikoardell@gmail.com>
2023-03-10 16:29:26 -0800

    prints the number of arguments passed into it

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main - prints the product of two integers.
+ * main - prints the sum of integers.
  * @argc: number of commands entered in argv
  * @argv: value of commands entered
  * Return: Always 0
@@ -10,17 +10,21 @@
 
 int main(int argc, char *argv[])
 {
-int i, mul = 1;
+int i, sum = 0;
 
 if (argc <= 1)
 {
-printf("Error\n");
+printf("0\n");
 }
-
 for (i = 1; i < argc; i++)
 {
-mul *= atoi(argv[i]);
+if (argv[i] < 48 || argv[i] > 57)
+{
+printf("Error\n");
+return 1;
 }
-printf("%d\n", mul);
-return (1);
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
 }
