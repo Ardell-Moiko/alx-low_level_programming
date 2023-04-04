7cc32e0d70d34b1de0a14c96c7355ca5e68a1896
Ardell Moiko <moikoardell@gmail.com>
2023-04-04 13:21:35 -0700

    adds a new node at the beginning of a linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,12 +11,12 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-if (head == NULL)
-return(NULL);
-
 listint_t *new_node = malloc(sizeof(listint_t));
 
-if (new_node == NULL);
+if (head == NULL)
+return (NULL);
+
+if (new_node == NULL)
 return (NULL);
 
 new_node->n = n;
