864e5d4b4599f21cc74eb9e8eec328c89a0348d6
Ardell Moiko <moikoardell@gmail.com>
2023-04-04 18:08:52 -0700

    getting segmentation fault on all files

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a linked list
+ * and returns the head node’s data
+ * @head: pointer to head node.
+ * Return: data of head node.
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *ptr = *head;
+int n;
+
+if (*head == NULL)
+return (0);
+
+n = ptr->n;
+ptr = ptr->next;
+free(ptr);
+return (n);
+}
