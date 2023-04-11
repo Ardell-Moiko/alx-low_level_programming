3656104b130d6dbbb17964bc04e2cbc1eab0a1cc
Ardell Moiko <moikoardell@gmail.com>
2023-04-10 17:58:28 -0700

    prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: long integar var
+ * Return: nothing.
+ */
+
+void print_binary(unsigned long int n)
+{
+unsigned long int lz = 1, b = 1;
+
+for (b = b << (sizeof(unsigned long int) * 8 - 1); b; b = b >> 1)
+{
+if ((n & b) && lz)
+lz = 0;
+
+if (lz == NULL)
+_putchar(((n & b) ? 1 : 0) + '0');
+}
+if (lz != NULL)
+_putchar('0');
+}
