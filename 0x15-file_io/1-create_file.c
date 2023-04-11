fda5863b0b2f452627e3380c5cd5e94453c21f20
Ardell Moiko <moikoardell@gmail.com>
2023-04-10 19:09:01 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+ssize_t f_des, buff, len = 0;
+
+if (!filename)
+return (-1);
+
+f_des = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (f_des == -1)
+return (-1);
+
+if (text_content)
+{
+while (text_content[i])
+len++;
+
+buff = write(f_des, text_content, len);
+
+if (buff == -1)
+return (-1)
+}
+close(f_des);
+return (1);
+}
