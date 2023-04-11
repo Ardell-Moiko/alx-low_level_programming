e4981e34dcb74a55611919e562af9b7c00906b63
Ardell Moiko <moikoardell@gmail.com>
2023-04-10 18:02:16 -0700

    prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -16,9 +16,9 @@ for (b = b << (sizeof(unsigned long int) * 8 - 1); b; b = b >> 1)
 if ((n & b) && lz)
 lz = 0;
 
-if (lz == NULL)
+if (!lz)
 _putchar(((n & b) ? 1 : 0) + '0');
 }
-if (lz != NULL)
+if (lz)
 _putchar('0');
 }
